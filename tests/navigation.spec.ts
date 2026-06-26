052ca7e64d2080840bf0ea165ad6c4adea5b4d38
Edwin Casallas <casallas22@hotmail.com>
2026-06-26 15:31:36 +0700

    Reto 5 Completado

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -78,4 +78,77 @@ test.describe('Verify left menu options', () => {
 
     })
 
+
+    test('Check all the qualification links', async ({ page }) => {
+        const expectedPages = [
+            {
+                menu: 'Skills',
+                url: '/web/index.php/admin/viewSkills'
+            },
+            {
+                menu: 'Education',
+                url: '/web/index.php/admin/viewEducation'
+            },
+            {
+                menu: 'Licenses',
+                url: '/web/index.php/admin/viewLicenses'
+            }
+        ]
+
+        await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+        await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+        await page.getByRole('button', { name: 'Login' }).click()
+
+        await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+        await page.getByRole('link', { name: 'Admin' }).click()
+        await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Qualifications').click()
+
+        const qualificationOptions = page.getByRole('menu').locator('li')
+
+        for (let expectedPage of expectedPages) {
+            const menuOption = qualificationOptions.filter({ hasText: expectedPage.menu })
+            await menuOption.click()
+            await expect(page).toHaveURL(new RegExp(expectedPage.url))
+            await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Qualifications').click()
+        }
+
+    })
+
+    test('Check all the Configuration links', async ({ page }) => {
+        const expectedPages = [
+            {
+                menu: 'Email Configuration',
+                url: '/web/index.php/admin/listMailConfiguration'
+            },
+            {
+                menu: 'Email Subscriptions',
+                url: '/web/index.php/admin/viewEmailNotification'
+            },
+            {
+                menu: 'Localization',
+                url: '/web/index.php/admin/localization'
+            }
+        ]
+
+        await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+        await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+        await page.getByRole('button', { name: 'Login' }).click()
+
+        await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+        await page.getByRole('link', { name: 'Admin' }).click()
+        await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Configuration').click()
+
+        const configurationOptions = page.getByRole('menu').locator('li')
+
+        for (let expectedPage of expectedPages) {
+            const menuOption = configurationOptions.filter({ hasText: expectedPage.menu })
+            await menuOption.click()
+            await expect(page).toHaveURL(new RegExp(expectedPage.url))
+            await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Configuration').click()
+        }
+
+    })
+
 })
