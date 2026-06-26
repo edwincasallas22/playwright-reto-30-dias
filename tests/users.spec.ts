4e40402d3fb3f76f295a29f8b41687b3b25c1f9d
Edwin Casallas <casallas22@hotmail.com>
2026-06-26 19:35:15 +0700

    Reto 6 Completado

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -62,4 +62,47 @@ test.describe('Verify HRM page', () => {
         console.log(employeeNames)
 
     })
+
+
+
+    test('Select random users except Admin for edition', async ({ page }) => {
+
+        await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+        await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+        await page.getByRole('button', { name: 'Login' }).click()
+
+        await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+        await page.getByRole('link', { name: 'Admin' }).click()
+        await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('User Management').click()
+        await page.getByRole('menuitem', { name: 'Users' }).click()
+
+        const validRows = page
+            .getByRole('table')
+            .getByRole('row')
+            .filter({ has: page.locator('button') })
+            .filter({ hasNotText: 'Admin' })
+
+        const rowCount = await validRows.count()
+        const randomIndex = Math.floor(Math.random() * rowCount);
+
+        const selectedRow = validRows.nth(randomIndex);
+
+        const expectedUserName = await selectedRow
+            .getByRole('cell')
+            .nth(1)
+            .innerText();
+
+        const pencilToEdit = selectedRow
+            .locator('button')
+            .filter({ has: page.locator('i.bi-pencil-fill') })
+
+        await pencilToEdit.click()
+
+        const currentUsername = page.locator("//label[contains(.,'Username')]/parent::div/following-sibling::div/input")
+        await expect(currentUsername).toBeVisible({ timeout: 10000 });
+
+        //   expect(await currentUsername.inputValue()).toEqual(userForEdition)
+        await expect(currentUsername).toHaveValue(expectedUserName)
+    })
 })
