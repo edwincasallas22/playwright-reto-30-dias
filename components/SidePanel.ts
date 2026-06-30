26566a02bd9f7376438b10ed6e33962fa69dc9e4
Edwin Casallas <casallas22@hotmail.com>
2026-06-30 20:04:07 +0700

    Reto 8 Completado

---
+++ components/SidePanel.ts
@@ -0,0 +1,40 @@
+import { Locator, Page } from "@playwright/test";
+
+
+
+export class SidePanel {
+
+    readonly page: Page
+
+    constructor(page: Page) {
+        this.page = page
+
+    }
+
+    private menuOption(option: SideMenuOption): Locator {
+        return this.page.getByRole('link', { name: option })
+    }
+
+    async clickOnOption(option: SideMenuOption) {
+        await this.menuOption(option).click()
+    }
+
+    async getMenuOption(option: SideMenuOption): Promise<String> {
+        return await this.menuOption(option).innerText()
+    }
+}
+
+export enum SideMenuOption {
+    ADMIN = 'Admin',
+    PIM = 'PIM',
+    LEAVE = 'Leave',
+    TIME = 'Time',
+    RECRUITMENT = 'Recruitment',
+    MY_INFO = 'My info',
+    PERFORMANCE = 'Performance',
+    DASHBOARD = 'Dashboard',
+    DIRECTORY = 'Directory',
+    MAINTENANCE = 'Maintenance',
+    CLAIM = 'Claim',
+    BUZZ = 'Buzz'
+}
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,42 +1,58 @@
-import {expect, test} from '@playwright/test'
+import { expect, test } from '@playwright/test'
+import { LoginPage } from '../pageobjects/LoginPage'
+import { SideMenuOption, SidePanel } from '../components/SidePanel'
 
-test.describe('Verify HRM page', () =>{
-    test.beforeEach(async({page}) =>{
-        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+test.describe('Verify HRM page', () => {
+
+
+    test('Verify successful login', async ({ page }) => {
+
+        const loginPage = new LoginPage(page)
+        await loginPage.doLogin('Admin', 'admin123')
+
+        const sidePanel = new SidePanel(page)
+        await sidePanel.clickOnOption(SideMenuOption.RECRUITMENT)
+        await sidePanel.clickOnOption(SideMenuOption.BUZZ)
+        await sidePanel.clickOnOption(SideMenuOption.LEAVE)
+        await sidePanel.clickOnOption(SideMenuOption.MY_INFO)
+        await sidePanel.clickOnOption(SideMenuOption.PIM)
+
+        await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
     })
 
+    test('Enter invalid credentials', async ({ page }) => {
 
-test('Verify successful login', async({page}) =>{    
-    await page.getByRole('textbox', {name:'Username'}).fill('Admin')
-    await page.getByRole('textbox', {name:'Password'}).fill('admin123')
-    await page.getByRole('button', {name:'Login'}).click()
+        const loginPage = new LoginPage(page)
+        await loginPage.doLogin('User', 'User123')
 
-    await expect(page.getByRole('link', {name:'Admin'})).toBeVisible()
-})
+        await expect(page.getByRole('alert')).toHaveText('Invalid credentials')
+    })
 
-test('Enter invalid credentials', async({page}) =>{
-    
-    await page.getByRole('textbox', {name:'Username'}).fill('User')
-    await page.getByRole('textbox', {name:'Password'}).fill('User123')
-    await page.getByRole('button', {name:'Login'}).click()
+    test('Enter special characters', async ({ page }) => {
 
-    await expect(page.getByRole('alert')).toHaveText('Invalid credentials')
-})
+        const loginPage = new LoginPage(page)
+        await loginPage.doLogin('!"#$%&/()', '!"#$%&/()')
 
-test('Enter special characters', async({page}) =>{
-    
-    await page.getByRole('textbox', {name:'Username'}).fill('!"#$%&/()')
-    await page.getByRole('textbox', {name:'Password'}).fill('!"#$%&/()')
-    await page.getByRole('button', {name:'Login'}).click()
+        await expect(page.getByText('Invalid credentials')).toBeVisible()
+    })
 
-    await expect(page.getByText('Invalid credentials')).toBeVisible()
-})
+    test('Verify empty fields', async ({ page }) => {
 
-test('Verify empty fields', async({page}) =>{
-    
-    await page.getByRole('button', {name:'Login'}).click()
+        const loginPage = new LoginPage(page)
+        await loginPage.doLogin('', '')
 
-    await expect(page.getByText('Required')).toHaveCount(2)
-})
+        await expect(page.getByText('Required')).toHaveCount(2)
+    })
+
+    test('Verify input search', async ({ page }) => {
+
+        const loginPage = new LoginPage(page)
+        await loginPage.doLogin('Admin', 'admin123')
+
+        await page.getByRole('textbox', { name: 'Search' }).fill(SideMenuOption.LEAVE)
+
+        const result = await page.getByLabel('Sidepanel').locator('ul.oxd-main-menu').innerText()
+        expect(result).toEqual(SideMenuOption.LEAVE)
+    })
 
 })
